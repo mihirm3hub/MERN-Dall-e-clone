9e76b69c3bc203664222f1ec8a2849703c544fbc
mihir <mihirmainkarm3@gmail.com>
2023-05-22 12:16:02 +0530

    MERN-DALL-E-CLN-v0.02: Update GET & POST request with render server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGenratingImg(true)
-        const response = await fetch('http://localhost:8080/api/v1/dalle',
+        const response = await fetch('https://dall-e-1bmg.onrender.com/api/v1/dalle',
           {
             method: 'POST',
             headers: {
@@ -47,7 +47,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true)
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-1bmg.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
