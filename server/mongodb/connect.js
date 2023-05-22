aa1cb79de7cccf19843c9720fab4368967707fce
mihir <mihirmainkarm3@gmail.com>
2023-05-22 09:45:01 +0530

    MERN-DALL-E-CLN-v0.01

---
+++ server/mongodb/connect.js
@@ -0,0 +1,12 @@
+//config mongodb
+import mongoose from "mongoose";
+
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true)
+
+    mongoose.connect(url)
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err))
+}
+
+export default connectDB
